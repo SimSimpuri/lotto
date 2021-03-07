83f2269dd77940204d850f1017cff03518f8147e
SimSimpuri <78592880+SimSimpuri@users.noreply.github.com>
2021-03-07 11:40:45 +0900

    Main layout commit

--- app/src/main/java/com/example/lotto/MainActivity.kt
+++ app/src/main/java/com/example/lotto/MainActivity.kt
@@ -1,14 +1,29 @@
 package com.example.lotto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        rand.setOnClickListener {
+            startActivity(Intent(this@MainActivity, RandomActivity::class.java))
+        }
+        constell.setOnClickListener{
+            startActivity(Intent(this@MainActivity, ConstellActivity::class.java))
+        }
+
+        name.setOnClickListener {
+            startActivity(Intent(this@MainActivity, NameActivity::class.java))
+        }
+
+        }
+
 
 
     }
-}
